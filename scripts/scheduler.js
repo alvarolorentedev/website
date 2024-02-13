e4def14961ef628708f496fc246ddb517ec87888
kanekotic <me@kanekotic.com>
2024-02-13 16:57:01 +0100

    feat: cleanup scheduler

--- scripts/scheduler.js
+++ scripts/scheduler.js
@@ -8,22 +8,10 @@ fs.readdirSync(temporalFolder).forEach(file => {
   console.log(file);
   fs.readFile(`${temporalFolder}${file}`, 'utf8', function(err, data){
     if (err) throw err
-   
     const {attributes} = fm(data)
-    console.log(attributes, !attributes, attributes.draft, attributes.published > Date.now() )
     if(!attributes || attributes.draft || attributes.published > Date.now() )
         return
-    console.log("move file")
     fs.renameSync(`${temporalFolder}${file}`,`${destinationFolder}${file}`)
 
   })
 });
- /*
-fs.readFile('./example.md', 'utf8', function(err, data){
-  if (err) throw err
- 
-  var content = fm(data)
- 
-  console.log(content)
-})
-*/
